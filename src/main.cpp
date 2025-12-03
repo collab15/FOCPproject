0c7f08e5b7254ff7b1d15b22de3f554469ce3c92
collab15 <project.collab.15@gmail.com>
2025-12-03 02:38:33 -0800

    Unstable commit

--- src/main.cpp
+++ src/main.cpp
@@ -27,7 +27,7 @@ int main() {
 
     // Prepare insert statement once (throws on failure)
     try {
-        db.prepareStatement("insertEvent", "INSERT INTO events (id, name, venue) VALUES ($1, $2, $3);", 3);
+        db.prepareStatement("insertEvent", "INSERT INTO events (event_id, name, venue, created_at,date, time, optional_details, organization_id) VALUES ($1, $2, $3 ,$4,$5,$6,$7,$8);", 3);
         cout << "Prepared statement successfully." << endl;
     }
     catch (const runtime_error& e) {
@@ -36,7 +36,7 @@ int main() {
     }
 
     // Route: /create-event
-    app().registerHandler(
+    drogon::app().registerHandler(
         "/create-event",
         [&db](const HttpRequestPtr& req, function<void(const HttpResponsePtr&)>&& callback) {
             auto resp = HttpResponse::newHttpResponse();
@@ -54,14 +54,41 @@ int main() {
                 //string name = (*json)["name"].asString();
                 //string venue = (*json)["venue"].asString();
 
-                string name = req->getParameter("name");
+               
                 string venue = req->getParameter("venue");
+                string time = req->getParameter("time");
+                string date = req->getParameter("date");
+                string optional_details = req->getParameter("optional_details");
+                string username = req->getParameter("username");
+                string password = req->getParameter("password");
 
-                if (name.empty() || venue.empty()) {
+                db.prepareStatement(
+                    "getOrg",
+                    "SELECT organization_id FROM organizations WHERE username = $1 AND password = $2;",
+                    2
+                );
+
+                auto rows = db.execPrepared("getOrg", { username, password });
+
+                if (rows.empty()) {
+                    resp->setStatusCode(k401Unauthorized);
+                    resp->setBody("Invalid organization name or password");
+                    return;
+                }
+
+                std::string orgId = rows[0][0];
+
+          
+
+                
+               
+
+                if (username.empty() || venue.empty() || time.empty() || date.empty() || optional_details.empty()) {
                     resp->setStatusCode(k400BadRequest);
-                    resp->setBody("Missing 'name' or 'venue'");
+                    resp->setBody("Missing detail'");
                     callback(resp);
                     return;
+
                 }
 
 
@@ -69,7 +96,7 @@ int main() {
 
                 // Insert into DB (throws on failure)
                 try {
-                    db.execPrepared("insertEvent", { eventId, name, venue });
+                    db.execPrepared("insertEvent", { eventId, username, venue, time, date, optional_details, orgId });
                 }
                 catch (const runtime_error& e) {
                     resp->setStatusCode(k500InternalServerError);
@@ -78,15 +105,6 @@ int main() {
                     return;
                 }
 
-                try{
-					QRPDFGenerator::generatePDF(eventId, name, venue, "ticket.pdf");
-                }
-                catch(const runtime_error& e){
-                    resp->setStatusCode(k500InternalServerError);
-                    resp->setBody(string("Ticket Creation failed: ") + e.what());
-                    callback(resp);
-                    return;
-                }
 
                 // Success
                 resp->setStatusCode(k200OK);
@@ -102,8 +120,92 @@ int main() {
         }
     );
 
-    app().addListener("0.0.0.0", myPort);
-    app().run();
+    
+    try {
+        db.prepareStatement("insertTicket", "INSERT INTO tickets (id, name, email, event_id, expiry) VALUES ($1, $2, $3, $4, $5);", 5);
+        cout << "Prepared statement successfully." << endl;
+    }
+    catch (const runtime_error& e) {
+        cerr << "Failed to prepare statement: " << e.what() << endl;
+        return 1; 
+    }
+
+    
+    drogon::app().registerHandler(
+        "/create-ticket",
+        [&db](const HttpRequestPtr& req, function<void(const HttpResponsePtr&)>&& callback) {
+            auto resp = HttpResponse::newHttpResponse();
+
+            try {
+
+            
+                string name = req->getParameter("name");
+                string email = req->getParameter("email");
+                string event_id = req->getParameter("event_id");
+                
+
+               
+                
+                
+
+
+                if (name.empty() || email.empty() || event_id.empty()) {
+                    resp->setStatusCode(k400BadRequest);
+                    resp->setBody("Missing 'name' or 'id' or 'email' or 'event_id' or 'expiry'");
+                    callback(resp);
+                    return;
+                }
+
+                string id = generate_uuid();
+
+
+               
+
+               
+                try {
+                    db.execPrepared("insertTicket", { id, name, email, event_id });
+                }
+                catch (const runtime_error& e) {
+                    resp->setStatusCode(k500InternalServerError);
+                    resp->setBody(string("Event Creation failed: ") + e.what());
+                    callback(resp);
+                    return;
+                }
+
+
+                resp->setStatusCode(k200OK);
+                resp->setBody("Ticket created successfully! \nTicket Id: " + id);
+            }
+            catch (const exception& e) {
+              
+                resp->setStatusCode(k500InternalServerError);
+                resp->setBody(string("Internal Server error: ") + e.what());
+            }
+
+
+            callback(resp);
+        }
+    );
+
+    drogon::app().registerHandler(
+        "/create-ticket",
+        [&db](const HttpRequestPtr& req, function<void(const HttpResponsePtr&)>&& callback) {
+            auto resp = HttpResponse::newHttpResponse();
+
+            try {
+                QRPDFGenerator::generatePDF("abc", "adc", "ff", "hh");
+            }
+            catch (const exception& e) {
+                cout << e.what();
+            }
+
+
+            callback(resp);
+        }
+    );
+
+    drogon::app().addListener("0.0.0.0", myPort);
+    drogon::app().run();
 
     return 0;
 }
